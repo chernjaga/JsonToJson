991dc628c579577c0f8425874a3f74c77166a47e
Aliaksandr Limasau <aliaksandr.limasau@wolterskluwer.com>
2024-05-28 22:32:57 +0400

    folder support

--- app/index.js
+++ app/index.js
@@ -5,25 +5,39 @@ const sourceJsonFolder = 'inputFiles';
 const outputFolder = 'outputFiles';
 const schemeFolder = 'schemes';
 const jsonTestRegexp = /\.json$/;
-const inputFiles = fs.readdirSync(sourceJsonFolder).filter(filename => jsonTestRegexp.test(filename));
 const schemes = fs.readdirSync(schemeFolder).filter(filename => jsonTestRegexp.test(filename));
 
 const redTextColor = '\u001b[31m';
 const resetTextColor = '\x1b[0m';
 
-schemes.forEach(scheme => {
-  const schemeObj = require(path.join('..', schemeFolder, scheme));
-  
-  inputFiles.forEach(filename => {
-    try {
-      console.log(resetTextColor);
-      const jsonAddress = path.join('..', sourceJsonFolder, filename);
-      const sourceJson = require(jsonAddress);
-      console.log(`validator is started for ${jsonAddress} \n`);
-      fs.writeFileSync(path.join(outputFolder, `converted_${filename}`), JSON.stringify(validator(sourceJson, schemeObj), null, 2));
-    } catch (error) {
-      console.error(`${redTextColor}ERROR in ${filename}: ${error.message}`);
-    }
-  })
+const getDirectories = source =>
+  fs.readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name);
+const directories = getDirectories(path.join(sourceJsonFolder));
+
+directories.forEach(directory => {
+  const dir = `${directory}`;
+  console.log(dir)
+  const inputFiles = fs.readdirSync(path.join(sourceJsonFolder, dir)).filter(filename => jsonTestRegexp.test(filename));
+  schemes.forEach(scheme => {
+    const schemeObj = require(path.join('..', schemeFolder, scheme));
+    
+    inputFiles.forEach(filename => {
+      try {
+        console.log(resetTextColor);
+        const jsonAddress = path.join('..', sourceJsonFolder, dir, filename);
+        const sourceJson = require(jsonAddress);
+        console.log(`validator is started for ${jsonAddress} \n`);
+        if (!fs.existsSync(path.join(outputFolder, dir))){
+          fs.mkdirSync(path.join(outputFolder, dir), { recursive: true });
+      }
+        fs.writeFileSync(path.join(outputFolder, dir, `converted_${filename}`), JSON.stringify(validator(sourceJson, schemeObj), null, 2));
+      } catch (error) {
+        console.error(`${redTextColor}ERROR in ${dir}/${filename}: ${error.message}`);
+      }
+    })
+  });
 });
+
 
